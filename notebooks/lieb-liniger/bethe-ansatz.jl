0652518ed4eaa73c345d1c85331586c90cecfa3b
Akshay <sakshays.2000@gmail.com>
2025-12-08 23:50:19 +0100

    add excitations

--- notebooks/lieb-liniger/bethe-ansatz.jl
+++ notebooks/lieb-liniger/bethe-ansatz.jl
@@ -268,6 +268,65 @@ function get_excitation_spectrum(γ; N=100)
     # return p_h, E_h, p_p, E_p
 end
 
+# ╔═╡ f43e514d-f3ce-4eca-bb4f-5289a29142f2
+function get_magnon_spectrum(γ; N=100)
+    c = 1.0
+    
+    _, _, n_total, Q = get_ground_state(γ; N=N)
+    k_fermi = π * n_total
+
+    Ksym(k, q) = c / π * (1/(c^2 + (k - q)^2) + 1/(c^2 + (k + q)^2))
+    solver = QuadratureSolver(gausslobatto(N))
+    
+    rho, xs, ws = solve(solver, Ksym, x -> 1/(2π), 0.0, Q)
+    rho_vals = rho.(xs)
+    
+    # solve for dressed energy epsilon
+    # (I-K)ε₀ = k^2  and  (I-K)ε₁ = 1
+    eps0_func, _, _ = solve(solver, Ksym, k -> k^2, 0.0, Q)
+    eps1_func, _, _ = solve(solver, Ksym, k -> 1.0, 0.0, Q)
+    
+    # determine chemical potential μ such that ε(Q) = 0
+    # ε(k) = ε₀(k) - μ * ε₁(k)
+    μ = eps0_func(Q) / eps1_func(Q)
+    eps_vals = eps0_func.(xs) .- μ .* eps1_func.(xs)
+
+    # compute magnon Spectrum
+    # scan spin rapidity Λ from 0 to ∞
+    # we use a denser grid near 0 where curvature is high
+    Λs = [range(0, 5.0, length=80); range(5.1, 50.0, length=20)]
+    
+    P_mag = Float64[]
+    E_mag = Float64[]
+    
+    # kernel for magnon coupling (Lorentzian of width c)
+    # K_coupling(x) = c/π * 1/(c^2 + x^2) 
+    K_coupling(x) = (c / π) * (1 / (c^2 + x^2))
+    
+    theta(x) = 2 * atan(x/c)
+    
+    for Λ in Λs
+        # energy: E = - ∫_{-Q}^Q K_coupling(k - Λ) ε(k) dk
+        term_E = K_coupling.(xs .- Λ) .+ K_coupling.(xs .+ Λ)
+        E = -dot(ws, term_E .* eps_vals)
+        
+        # momentum: P = k_F - ∫_{-Q}^Q θ(k - Λ) ρ(k) dk
+        term_P = theta.(xs .- Λ) .- theta.(xs .+ Λ)
+        P = k_fermi - dot(ws, term_P .* rho_vals)
+        
+        push!(P_mag, P)
+        push!(E_mag, E)
+    end
+
+    return P_mag, E_mag, k_fermi
+end
+
+# ╔═╡ 96531a98-4ad8-4e0d-b2a3-b36c085c87f2
+let
+	P, E, k = get_magnon_spectrum(10)
+	plot(P, E)
+end
+
 # ╔═╡ cae2cf42-03ee-4db8-b8a1-31a8d51a7335
 let
 	# TG limit
@@ -1532,6 +1591,8 @@ version = "1.9.2+0"
 # ╠═87f35899-f6dd-4728-8ef7-b70a65c95dfd
 # ╠═1993226e-c10e-4416-9b5d-de75f7f1e461
 # ╠═dc279c09-599e-4dd8-a0d8-d79c233d8e47
+# ╠═f43e514d-f3ce-4eca-bb4f-5289a29142f2
+# ╠═96531a98-4ad8-4e0d-b2a3-b36c085c87f2
 # ╠═cae2cf42-03ee-4db8-b8a1-31a8d51a7335
 # ╠═7be5d546-88d0-4325-9645-f8d02d1d4864
 # ╟─00000000-0000-0000-0000-000000000001
